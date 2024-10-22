c6d9a2a8705362cddf67f4c2ab0d8f27d9cc63ec
minebinroyal1@gmail.com <minebinroyal1@gmail.com>
2024-10-22 22:37:35 +0700

    razorWebPage Studied

---
+++ BulkyWebRazor_Temp/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,39 @@
+using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWebRazor_Temp.Pages.Categories
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public Category Category { get; set; }
+
+        public DeleteModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet(int? Id)
+        {
+            if(Id != null && Id != 0)
+            {
+                Category = _db.Categories.FirstOrDefault(u => u.Id == Id);
+            }
+            
+        }
+        public IActionResult OnPost()
+        {
+
+            if (Category == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(Category);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToPage("Index");
+        }
+    }
+}
